432ae52a72ae0df568238ac3ec54b7c3857e5127
Apinan Woratrakun <iamapinan@gmail.com>
2024-10-03 12:11:55 +0700

    Update UploadController.php

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -21,7 +21,7 @@ class UploadController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        //$this->middleware(['auth', 'verified']);
     }
 
     /**
